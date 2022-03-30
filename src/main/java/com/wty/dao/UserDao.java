e6bb0abd906e39400c8a070bdc23c9e8f6f84a64
wangtaoyuan <hdwangtaoyuan@qq.com>
2022-03-30 23:41:56 +0800

    Init Spring product

---
+++ src/main/java/com/wty/dao/UserDao.java
@@ -0,0 +1,5 @@
+package com.wty.dao;
+
+public interface UserDao {
+    public void save();
+}
---
+++ src/main/java/com/wty/dao/impl/UserDaoImpl.java
@@ -0,0 +1,9 @@
+package com.wty.dao.impl;
+
+import com.wty.dao.UserDao;
+
+public class UserDaoImpl implements UserDao {
+    public void save() {
+        System.out.println("saving...");
+    }
+}
