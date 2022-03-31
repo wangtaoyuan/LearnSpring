419cfa88a8d23c6b450853bba76ffc3e148895b8
wangtaoyuan <hdwangtaoyuan@qq.com>
2022-03-31 23:49:15 +0800

    scope type: singleton, prototype

--- src/main/java/com/wty/dao/impl/UserDaoImpl.java
+++ src/main/java/com/wty/dao/impl/UserDaoImpl.java
@@ -3,6 +3,10 @@ package com.wty.dao.impl;
 import com.wty.dao.UserDao;
 
 public class UserDaoImpl implements UserDao {
+    // 重写无参构造函数
+    public UserDaoImpl() {
+        System.out.println("UserDaoImpl building...");
+    }
     public void save() {
         System.out.println("saving...");
     }
